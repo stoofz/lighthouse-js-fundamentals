4c24b2ee295ba7550d74f0e950f154a13324a27f
Anthony Chambers <ajfchambers@gmail.com>
2023-01-25 15:57:20 -0500

    ice-cream.js created

---
+++ ice-cream.js
@@ -0,0 +1,33 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push("root beer");
+
+console.log(iceCreamFlavours);
+console.log(iceCreamFlavours[0]);
+console.log(iceCreamFlavours[5]);
+console.log(iceCreamFlavours.length);
+
+console.log("15" === 15);
+console.log("Bob" || " Marley")
+console.log("Jeremy" > "Khurram")
+
+let sum = 3 + 2;
+
+const doubleSum = function(a, b) {
+  let sum = a + b;
+  return sum * 2;
+}
+
+doubleSum(5, 4);
+
+console.log(sum);
+const seasons = ["spring", "summer", "fall"];
+const length = seasons.length;
+
+seasons.push("winter");
+
+console.log(length);
+console.log(seasons.length);
